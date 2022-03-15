3835145011a13aac571a061fd40fa6cd77a1a9ec
Christopher N. Hesse <raymanfx@gmail.com>
2022-03-15 12:12:59 +0100

    ginkgo: Enforce common device & vendor tree presence
    A compiled build has no chance of booting without the common device setup
    and the prebuilt binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@
 #
 
 # Inherit from sm6125-common
--include device/xiaomi/sm6125-common/BoardConfigCommon.mk
+include device/xiaomi/sm6125-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/ginkgo
 
@@ -61,4 +61,4 @@ VENDOR_SECURITY_PATCH := 2019-10-05
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit the proprietary files
--include vendor/xiaomi/ginkgo/BoardConfigVendor.mk
+include vendor/xiaomi/ginkgo/BoardConfigVendor.mk
