83cb1534b213de4c6c2a92eeea3a71d3e03278be
RahulPalXDA <RahulPal.XDA@gmail.com>
2022-05-02 09:03:45 +0530

    Import lavender lineage device configuration

---
+++ BoardConfig.mk
@@ -0,0 +1,70 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Inherit from sdm660-common
+-include device/xiaomi/sdm660-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/lavender
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := lavender
+
+# Android Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Display
+TARGET_SCREEN_DENSITY := 420
+
+# Kernel
+TARGET_KERNEL_CONFIG := lavender_defconfig
+BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
+
+# Manifest
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/manifest.xml
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3640619008
+BOARD_VENDORIMAGE_PARTITION_SIZE := 2080305152
+BOARD_DTBOIMG_PARTITION_SIZE := 8388608
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2019-10-05
+
+# Inherit the proprietary files
+-include vendor/xiaomi/lavender/BoardConfigVendor.mk
